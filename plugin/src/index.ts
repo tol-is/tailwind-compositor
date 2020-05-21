82b7e950fd8ef805fbedace6be6539dc237f22dc
tol.is <me@tol.is>
2020-05-22 00:39:18 +0100

    plugin

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,6 +1,2 @@
 export * from './tailwind-compositor';
-export * from './theme-merge';
-// const compose = require('./theme-compose');
-
-// exports.compose = compose;
-// exports.plugin = plugin;
+export * from './theme-transform';
