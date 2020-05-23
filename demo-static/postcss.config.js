5be873b42c34d1709a53bdb89a5216fb1edcda63
tol.is <me@tol.is>
2020-05-23 21:59:27 +0100

    trying to make demo work

--- demo-static/postcss.config.js
+++ demo-static/postcss.config.js
@@ -1,13 +1,22 @@
 const tailwindcss = require('tailwindcss');
-const { transform } = require('tailwind-compositor');
+// const { compositor } = require('tailwind-compositor');
 
-const compositorConfig = require('./compositor.config.js');
+// const compositorConfig = require('./compositor.config.js');
 const tailwindConfig = require('./tailwind.config.js');
 
-const transformer = transform(compositorConfig);
-
-console.log(transformer(tailwindConfig));
-
+console.log('asds');
+// const tailwindConfigComposed = compositor(compositorConfig)(tailwindConfig);
+// debugger;
 module.exports = {
-	plugins: [tailwindcss(transformer(tailwindConfig))],
+	plugins: [
+		tailwindcss(tailwindConfig),
+		require('postcss-preset-env')({
+			stage: 1,
+			autoprefixer: { grid: true },
+			features: {
+				'nesting-rules': true,
+			},
+			browsers: ['> 1%', 'last 2 versions', 'Firefox ESR'],
+		}),
+	],
 };
