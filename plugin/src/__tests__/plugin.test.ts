e40ea57b72f8a4e28a32a60d7d30fe89e283a13b
tol.is <me@tol.is>
2020-05-25 17:09:24 +0100

    enable jest tests

--- plugin/src/__tests__/plugin.test.ts
+++ plugin/src/__tests__/plugin.test.ts
@@ -4,132 +4,131 @@ test('simple transform', () => {
 	expect(true).toBe(true);
 });
 
-// import postcss from 'postcss';
-// import tailwindcss from 'tailwindcss';
+import postcss from 'postcss';
+import tailwindcss from 'tailwindcss';
 
-// import { merge } from '../utils';
-// import compositor from '../theme-compositor';
+import { merge } from '../utils';
+import compositor from '../theme-compositor';
 
-// import compositorBaseConfig from './fixtures/compositor.config.js';
-// import tailwindConfig from './fixtures/tailwind.config.js';
+import compositorBaseConfig from './fixtures/compositor.config.js';
+import tailwindConfig from './fixtures/tailwind.config.js';
 
-// const createPostCSSConfig = ({ config, input = '@tailwind utilities;' }) => {
-// 	// make config
-// 	const compositorConfig = merge(compositorBaseConfig, config);
+const createPostCSSConfig = ({ config, input = '@tailwind utilities;' }) => {
+	// make config
+	const compositorConfig = merge(compositorBaseConfig, config);
 
-// 	// transform tailwind theme
-// 	const tailwindConfigComposed = compositor(compositorConfig)(tailwindConfig);
+	// transform tailwind theme
+	const tailwindConfigComposed = compositor(compositorConfig)(tailwindConfig);
 
-// 	// run postcss
-// 	return postcss([tailwindcss(tailwindConfigComposed)])
-// 		.process(input, {
-// 			from: undefined,
-// 		})
-// 		.then(({ css }) => {
-// 			return css;
-// 		});
-// };
+	// run postcss
+	return postcss([tailwindcss(tailwindConfigComposed)])
+		.process(input, {
+			from: undefined,
+		})
+		.then(({ css }) => {
+			return css;
+		});
+};
 
-// test('use rem', () => {
-// 	const config = {
-// 		useRem: true,
-// 	};
-// 	return createPostCSSConfig({ config: config }).then(css => {
-// 		expect(css).toMatchSnapshot();
-// 	});
-// });
+test('use rem', () => {
+	const config = {
+		useRem: true,
+	};
+	return createPostCSSConfig({ config: config }).then(css => {
+		expect(css).toMatchSnapshot();
+	});
+});
 
-// test('use px', () => {
-// 	const config = {
-// 		useRem: false,
-// 	};
-// 	return createPostCSSConfig({ config: config }).then(css => {
-// 		expect(css).toMatchSnapshot();
-// 	});
-// });
+test('use px', () => {
+	const config = {
+		useRem: false,
+	};
+	return createPostCSSConfig({ config: config }).then(css => {
+		expect(css).toMatchSnapshot();
+	});
+});
 
-// test('baseline styles', () => {
-// 	const config = {
-// 		useRem: false,
-// 		options: {
-// 			xray: true,
-//      baseline: true,
-// 			capheight: false,
-// 			xheight: false,
-// 			rhythm: false,
-// 			measure: false,
-// 		},
-// 	};
-// 	return createPostCSSConfig({ config: config }).then(css => {
-// 		expect(css).toMatchSnapshot();
-// 	});
-// });
+test('baseline styles', () => {
+	const config = {
+		useRem: false,
+		options: {
+			xray: true,
+			baseline: true,
+			capheight: false,
+			xheight: false,
+			rhythm: false,
+			measure: false,
+		},
+	};
+	return createPostCSSConfig({ config: config }).then(css => {
+		expect(css).toMatchSnapshot();
+	});
+});
 
-// test('capheight styles', () => {
-// 	const config = {
-// 		useRem: false,
-// 		options: {
-// 			xray: false,
-//      baseline: false,
-// 			capheight: true,
-// 			xheight: false,
-// 			rhythm: false,
-// 			measure: false,
-// 		},
-// 	};
-// 	return createPostCSSConfig({ config: config }).then(css => {
-// 		expect(css).toMatchSnapshot();
-// 	});
-// });
+test('capheight styles', () => {
+	const config = {
+		useRem: false,
+		options: {
+			xray: false,
+			baseline: false,
+			capheight: true,
+			xheight: false,
+			rhythm: false,
+			measure: false,
+		},
+	};
+	return createPostCSSConfig({ config: config }).then(css => {
+		expect(css).toMatchSnapshot();
+	});
+});
 
-// test('xheight styles', () => {
-// 	const config = {
-// 		useRem: false,
-// 		options: {
-// 			xray: false,
-// 			baseline: false,
-// 			capheight: false,
-// 			xheight: true,
-// 			rhythm: false,
-// 			measure: false,
-// 		},
-// 	};
-// 	return createPostCSSConfig({ config: config }).then(css => {
-// 		expect(css).toMatchSnapshot();
-// 	});
-// });
+test('xheight styles', () => {
+	const config = {
+		useRem: false,
+		options: {
+			xray: false,
+			baseline: false,
+			capheight: false,
+			xheight: true,
+			rhythm: false,
+			measure: false,
+		},
+	};
+	return createPostCSSConfig({ config: config }).then(css => {
+		expect(css).toMatchSnapshot();
+	});
+});
 
-// test('rhythm styles', () => {
-// 	const config = {
-// 		useRem: false,
-// 		options: {
-// 		  xray: false,
-// 			baseline: false,
-// 			capheight: false,
-// 			xheight: false,
-// 			rhythm: true,
-// 			measure: false,
-// 		},
-// 	};
-// 	return createPostCSSConfig({ config: config }).then(css => {
-// 		expect(css).toMatchSnapshot();
-// 	});
-// });
+test('rhythm styles', () => {
+	const config = {
+		useRem: false,
+		options: {
+			xray: false,
+			baseline: false,
+			capheight: false,
+			xheight: false,
+			rhythm: true,
+			measure: false,
+		},
+	};
+	return createPostCSSConfig({ config: config }).then(css => {
+		expect(css).toMatchSnapshot();
+	});
+});
 
-// test('measure styles', () => {
-// 	const config = {
-// 		useRem: false,
-// 		options: {
-// 		  xray: false,
-// 			baseline: false,
-// 			capheight: false,
-// 			xheight: false,
-// 			rhythm: false,
-// 			measure: true,
-// 		},
-// 		},
-// 	};
-// 	return createPostCSSConfig({ config: config }).then(css => {
-// 		expect(css).toMatchSnapshot();
-// 	});
-// });
+test('measure styles', () => {
+	const config = {
+		useRem: false,
+		options: {
+			xray: false,
+			baseline: false,
+			capheight: false,
+			xheight: false,
+			rhythm: false,
+			measure: true,
+		},
+	};
+	return createPostCSSConfig({ config: config }).then(css => {
+		expect(css).toMatchSnapshot();
+	});
+});
